03fc6c4e1859084b85539ad00bd658a833f15b69
yuliiaandriienko <youandrienko@gmail.com>
2022-01-26 21:26:28 +0200

    Initial commit

---
+++ Dublicate_Text/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dublicate_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Шапка програми
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("*****Мій проект****");
+            Console.WriteLine("********************");
+
+            // Основна програма
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            Console.WriteLine("Hello, World!");
+
+            // Блок коду, що дублює написаний текст
+            Console.WriteLine($"Введіть текст та натисніть enter");
+            var lineFromConsole = Console.ReadLine();
+            Console.WriteLine($"Виводимо строку яку ми раніше ввели в консоль: {lineFromConsole}");
+
+            Console.ReadLine();
+        }
+    }
+}
